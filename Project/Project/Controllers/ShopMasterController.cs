0aea168a712630a86a0aa64aab06fa7230e0785f
harshit11022@gmail.com <HarshitChilvirwar@puneintranet.rhealtech.com>
2024-05-13 19:28:34 +0530

    adding

---
+++ Project/Project/Controllers/ShopMasterController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+using Model;
+using DataAccessLayeProduct;
+using DataAccessLayeProductCategories;
+using CustomerModel;
+namespace Project.Controllers
+{
+    public class ShopMasterController : Controller
+    {
+        
+        #region
+        ShopModel shopModel = new ShopModel();
+        ShopMaster shopMaster = new ShopMaster();
+        DataLaye DataLayeProduct = new DataLaye();
+        DataLayeCategories dataLayeCategories = new DataLayeCategories();
+        CustomerDAL customerDAL = new CustomerDAL();
+        Customer customer = new Customer();
+        #endregion
+        // GET: ShopMaster
+        public ActionResult Index(ShopModel shopModel)
+        {
+            shopModel.CustomerName = shopMaster.CustomerName;
+            shopModel.CustomerId = shopMaster.CustomerId;
+            shopModel.ProductId = shopMaster.ProductId;
+            shopModel.ProductName = shopMaster.ProductName;
+            shopModel.ProductCategoriesId = shopMaster.ProductCategoriesId;
+            shopModel.ProductCategory = shopMaster.ProductCategory;
+            shopModel.ProductPrice = shopMaster.ProductPrice;
+            shopModel.TotalAmmount = shopMaster.TotalAmmount;
+            shopModel.PageNumber = shopMaster.PageNumber;
+            shopModel.PageSize = shopMaster.PageSize;
+            shopModel.FromDate = shopMaster.FromDate;
+            shopModel.ToDate = shopMaster.ToDate;
+            shopModel.MasterList = shopMaster.GetList();        
+            shopModel.ProductModelList = DataLayeProduct.GetList();
+            shopModel.ProductCategoryModel = dataLayeCategories.GetList();
+            return View(shopModel);
+        }
+       [HttpPost]
+        public ActionResult GetList(ShopModel shopModel)
+        {
+            shopMaster.CustomerName = shopModel.CustomerName;
+            shopMaster.CustomerId = shopModel.CustomerId;
+            shopMaster.ProductId = shopModel.ProductId;
+            shopMaster.ProductName = shopModel.ProductName;
+            shopMaster.ProductCategoriesId = shopModel.ProductCategoriesId;
+            shopMaster.ProductCategory = shopModel.ProductCategory;
+            shopMaster.ProductPrice = shopModel.ProductPrice;
+            shopMaster.TotalAmmount = shopModel.TotalAmmount;
+            shopMaster.PageNumber =shopModel.PageNumber;
+            shopMaster.PageSize =   shopModel.PageSize;
+            shopMaster.FromDate = shopModel.FromDate;
+            shopMaster.ToDate = shopModel.ToDate;
+            shopModel.MasterList = shopMaster.GetList();
+            shopModel.TotalCount = shopMaster.TotalCount;
+            shopModel.ProductModelList = DataLayeProduct.GetList();
+            shopModel.ProductCategoryModel = dataLayeCategories.GetList();
+            return Json(shopModel, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            shopModel.CustomerList = customerDAL.GetList(customer);
+            shopModel.ProductModelList = DataLayeProduct.GetList();
+            shopModel.ProductCategoryModel = dataLayeCategories.GetList();
+            return View ("_Insert",shopModel);
+           // return View(shopModel);
+        }
+        [HttpPost]
+        public ActionResult Insert(ShopModel shopModel)
+        {
+            //shopModel.CustomerList = customerDAL.GetList(customer);
+            //shopModel.ProductModelList = DataLayeProduct.GetList();
+            //shopModel.ProductCategoryModel = dataLayeCategories.GetList();         
+            shopMaster.CustomerId = shopModel.CustomerId;
+            shopMaster.ProductId = shopModel.ProductId;        
+            shopMaster.Quantity = shopModel.Quantity;
+            shopMaster.DateSaleOrders = shopModel.DateSaleOrders;
+            shopMaster.Save();
+            return Redirect("Index");
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            if (shopMaster.Delete(id))
+            {
+                return Json(new { Success = true, Message = "Item deleted successfully" });
+            }
+            else
+            {
+                return Json(new { Success = false, Message = "Item is not deleted" });
+            }
+        }
+        [HttpGet]
+        public ActionResult Edit(ShopModel shopModel)
+        {
+            shopMaster.SaleOrdersDetailsId = shopModel.SaleOrdersDetailsId;
+            shopMaster.Load();           
+            shopMaster.PageNumber = shopModel.PageNumber;
+
+            Session["PageNumber"] = shopModel.PageNumber;
+
+            shopModel.CustomerName = shopMaster.CustomerName;
+            shopModel.CustomerId =shopMaster.CustomerId;
+            shopModel.DateSaleOrders = shopMaster.DateSaleOrders;
+            shopModel.ProductName = shopMaster.ProductName;
+            shopModel.ProductId=shopMaster.ProductId;
+            shopModel.Quantity = shopMaster.Quantity;
+            shopModel.CustomerList = customerDAL.GetList(customer);
+            shopModel.ProductModelList = DataLayeProduct.GetList();
+            shopModel.ProductCategoryModel = dataLayeCategories.GetList();
+                    
+            //return View(shopModel);
+            return View("_Edit", shopModel);
+        }
+        [HttpPost]
+        //JsonResult
+        public JsonResult Edit(ShopModel shopModel,ShopMaster shopMaster)
+        {
+            
+            shopMaster.SaleOrdersDetailsId= shopModel.SaleOrdersDetailsId;    
+            shopMaster.CustomerId = shopModel.CustomerId;
+            shopMaster.ProductId = shopModel.ProductId;
+            shopMaster.DateSaleOrders = shopModel.DateSaleOrders;
+            shopModel.Quantity = shopMaster.Quantity;
+            shopModel.PageNumber = shopMaster.PageNumber;
+            shopModel.CustomerList = customerDAL.GetList(customer);
+            shopModel.ProductModelList = DataLayeProduct.GetList();
+            shopModel.ProductCategoryModel = dataLayeCategories.GetList();
+            shopMaster.Save();
+          //  return View("Index",shopModel);
+            
+          return Json(new {success=true, message="edit successfull " });
+        }
+    }
+}
---
+++ Project/Project/Scripts/JavaScriptOwn.js
@@ -0,0 +1,235 @@
+﻿
+    $(document).ready(function () {
+        var PageNumber = 1;
+        var PageSize = 10;
+        var CurrentPage = 0;
+        var GlobalTotalCount = 0;
+        var counter;
+        function GetList(CustomerName, ProductId, ProductCategoriesId, FromDate, ToDate, PageNumber, PageSize) {
+         var obj = {
+               CustomerName: CustomerName,
+               ProductId: ProductId,
+               ProductCategoriesId: ProductCategoriesId,
+               FromDate: FromDate,
+               ToDate: ToDate,
+               PageNumber: PageNumber,
+               PageSize: PageSize
+            }
+            $.ajax({
+             
+                url: "/ShopMaster/GetList",
+                    //'@Url.Action("GetList", "ShopMaster")',
+                method: "POST",
+                data: obj,
+                dataType: "json",
+                success: function (data) {
+                    // console.log(data)
+                    //data = JSON.parse(data);
+                  
+                    console.log("HEllo............")
+                    RenderList(data.MasterList)
+                    renderPagination(data.TotalCount, data.PageNumber);
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+        }
+        function Insert() {
+            $.ajax({
+                url: "/ShopMaster/Insert",
+                    //'@Url.Action("Insert", "ShopMaster")',
+                method: "GET",
+                success: function (response) {
+                    $('.modal-body').html(response);
+                },
+                error: function (xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
+        }
+        function Delete(id) {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                    var obj = { id: id };
+                    $.ajax({
+                        url: "/ShopMaster/Delete",
+                            //'@Url.Action("Delete","ShopMaster")',
+                        type: 'POST',
+                        data: obj,
+                        success: function () {
+                            console.log('Success..')
+                            GetList(null, null, null, null, null, PageNumber, PageSize);
+                        },
+                        error: function () {
+                            console.log(error);
+                        }
+                    })
+                }
+            })
+        }
+        function Edit(id) {
+            var obj = {
+                SaleOrdersDetailsId: id,
+                PageNumber: PageNumber 
+            }
+            $.ajax({
+                url: '/ShopMaster/Edit/ ' + id,
+                //'@Url.Action("Edit", "ShopMaster")',
+                method: 'GET',             
+                data: obj,                
+                success: function (response) {                
+                    $('.modal').modal();
+                    $('.modal-body').html(response);
+                    console.log('Edit Working... 2');
+                },
+                eror: function () {
+                    console.log(error);
+                }
+            })
+        }      
+        //function RenderList(list) {
+        //    var tableBody = $('#tblMasterShop tbody');
+        //    tableBody.empty();
+        //    $.each(list, function (index, item) {
+        //        var row = '<tr align="center" style="border: 1px solid #807d78;">' +
+        //            '<td scope="col" style="border: 1px solid #807d78;">' + item.CustomerName + '</td>' +
+        //            '<td scope="col" style="border: 1px solid #807d78;">' + item.DateSaleOrders + '</td>' +
+        //            '<td scope="col" style="border: 1px solid #807d78;">' + item.ProductName + '</td>' +
+        //            '<td scope="col" style="border: 1px solid #807d78;">' + item.ProductCategory + '</td>' +
+        //            '<td scope="col" style="border: 1px solid #807d78;">' + item.Quantity + '</td>' +
+        //            '<td scope="col" style="border: 1px solid #807d78;">' + item.ProductPrice + '</td>' +
+        //            '<td scope="col" style="border: 1px solid #807d78;">' + item.TotalAmmount + '</td>' +
+        //            '<td scope="col" style="border: 1px solid #807d78;">' + '<button id="EditSaleOrdersDetails" value="' + item.SaleOrdersDetailsId
+        //            + '" class="btn ">Edit</button>' + '&nbsp;&nbsp;' + '<button id = "deleteSaleOrder" value="' + item.SaleOrdersDetailsId
+        //            + '" class="btn" >Delete</button>' + '</td>'
+        //        tableBody.append(row);
+        //    }
+        //    )
+        //}
+        function renderPagination(TotalCount, PageNumber) {
+            $(".pagination").empty()
+            GlobalTotalCount = TotalCount;
+            if (PageNumber > 1) {
+                $(".pagination").append(`<li class="page-item "  value=${PageNumber - 1} id="pagenumber"><a class="page-link" href="#" tabindex="-1">Previous</a></li>`)
+                CurrentPage = $('#pagenumber').val();
+            }
+            for (var i = 1; i <= TotalCount; i++) {
+                if (PageNumber == i) {
+                    $(".pagination").append(` <li class="page-item active" value=${i} id="pagenumber" ><a class="page-link" href="#">${i}</a></li>`)
+                    CurrentPage = $('#pagenumber').val();
+                } else {
+                    $(".pagination").append(` <li class="page-item "   value=${i} id="pagenumber"><a class="page-link" href="#">${i}</a></li>`)
+                    CurrentPage = $('#pagenumber').val();
+                }
+            }
+            if (TotalCount != PageNumber) {
+                $(".pagination").append(`<li class="page-item"   value=${PageNumber + 1} id="pagenumber"><a class="page-link" href="#">Next</a></li>`)
+                CurrentPage = $('#pagenumber').val();
+            }
+        }
+        function search() {
+            event.preventDefault(); // Prevent default form submission
+            CustomerName = $('#CustomerName').val().trim();
+            ProductId = $('#ProductId').val();
+            ProductCategoriesId = $('#ProductCategoriesId').val();
+            FromDate = $('#FromDate').val();
+            ToDate = $('#ToDate').val();
+            PageSize = $('#PageSize').val();
+            PageNumber = 1;
+            console.log("Search Funtion PageSize " + PageSize)
+            console.log("Search Funtion PageNumber" + PageNumber)
+            GetList(CustomerName, ProductId, ProductCategoriesId, FromDate, ToDate, PageNumber, PageSize);
+        }
+        $('#reset').click(function () {
+            CustomerName = $('#CustomerName').val(null)
+            ProductId = $('#ProductId').val(null);
+            ProductCategoriesId = $('#ProductCategoriesId').val(null);
+            FromDate = $('#FromDate').val(null);
+            ToDate = $('#ToDate').val(null);
+            PageNumber = 1;
+            PageSize = $('#PageSize').val();
+            GetList(null, null, null, null, null, PageNumber, PageSize);
+        })
+        $('#PageSize').change(function () {
+            PageSize = $(this).val();
+            PageNumber = 1;
+            CustomerName = $('#CustomerName').val().trim();
+            ProductId = $('#ProductId').val();
+            ProductCategoriesId = $('#ProductCategoriesId').val();
+            FromDate = $('#FromDate').val();
+            ToDate = $('#ToDate').val();
+           // PageSize = $('#PageSize').val();
+            console.log("Search Funtion PageSize " + PageSize)
+            console.log("Search Funtion PageNumber" + PageNumber)
+            GetList(CustomerName, ProductId, ProductCategoriesId, FromDate, ToDate, 1, PageSize);
+        })
+        $('#insert').click(Insert);
+        $('#searchForm').submit(search);
+        $(document).on('click', '#EditSaleOrdersDetails', function () {
+            var id = $(this).val();          
+            Edit(id);
+        })
+        $(document).on('click', '#deleteSaleOrder', function () {
+            var id = $(this).val();
+            Delete(id);
+        })
+        $(document).on('click', '#pagenumber', function () {
+            console.log("Function get clicked..")
+            PageNumber = $(this).val();
+            CustomerName = $('#CustomerName').val().trim();
+            ProductId = $('#ProductId').val();
+            ProductCategoriesId = $('#ProductCategoriesId').val();
+            FromDate = $('#FromDate').val();
+            ToDate = $('#ToDate').val();
+            PageSize = $('#PageSize').val();
+            console.log("Search Funtion PageSize " + PageSize)
+            console.log("Search Funtion PageNumber" + PageNumber)
+            GetList(CustomerName, ProductId, ProductCategoriesId, FromDate, ToDate, PageNumber, PageSize);
+        })
+        
+        function editSave() {
+            var formData = $('#form').serialize();
+            var CustomerName = $('#CustomerName').val().trim();
+            var ProductId = $('#ProductId').val();
+            var ProductCategoriesId = $('#ProductCategoriesId').val();
+            var FromDate = $('#FromDate').val();
+            var ToDate = $('#ToDate').val();
+                PageSize = $('#PageSize').val();
+            console.log("this form data"+ formData)
+            debugger;
+            $.ajax({
+                url: "/ShopMaster/Edit",
+                //'@Url.Action("Edit", "ShopMaster")',
+                method: "POST",
+                data: formData,
+                dataType: "json",
+                success: function (response) {
+                    console.log("Page Number is ==>" + PageNumber)
+                    console.log("save btn log");
+                    GetList(CustomerName, ProductId, ProductCategoriesId, FromDate, ToDate, PageNumber, PageSize);
+                  //  GetList(null, null, null, null, null, PageNumber, PageSize);
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+        }
+        $(document).on('click', '#saveBtn', function () {
+            editSave();
+        })
+        GetList();
+    })
